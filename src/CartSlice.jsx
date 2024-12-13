4bc6fbec48eff0fa1d11897f65a2fbc7b7485330
Nana Aba Ackah <79535653+nanaabaackah@users.noreply.github.com>
2024-12-13 13:39:13 +0000

    update delete

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -16,7 +16,10 @@ export const CartSlice = createSlice({
       }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);     
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        state.items = state.items.filter(item => item.name !== action.payload);
+      }
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
